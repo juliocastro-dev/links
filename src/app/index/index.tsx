93b58cb50cc8950fd979fd7a43c1093731ca636d
Julio Castro <juliocastro.developer@gmail.com>
2024-10-26 12:42:41 -0300

    feat: create a Option component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,6 +5,7 @@ import { colors } from "@/styles/colors";
 
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 
 export default function Index () {
     return (
@@ -49,6 +50,10 @@ export default function Index () {
                         <Text style={styles.modalUrl}>
                             https://rockeatseat.com.br
                         </Text>
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary" />
+                            <Option name="Abrir" icon="language" />
+                        </View>
                     </View>
                 </View>
             </Modal>
